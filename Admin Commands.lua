de0ab581b0ce14bd209c166c685a4ee18e5040ba
No <113750837+Jayripsus@users.noreply.github.com>
2023-01-04 00:48:50 -0500

    Create Admin Commands.lua

---
+++ Admin Commands.lua
@@ -0,0 +1,299 @@
+local A_1 = "[Successfully loaded Commands will appear in this box below.]"-----message here
+local A_2 = "All"
+local Event = game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest
+Event:FireServer(A_1, A_2)
+-- Command information 
+-- Gui to Lua
+-- Version: Undetected
+local ScreenGui = Instance.new("ScreenGui")
+local Frame = Instance.new("Frame")
+local TextLabel = Instance.new("TextLabel")
+local TextLabel_2 = Instance.new("TextLabel")
+local TextLabel_3 = Instance.new("TextLabel")
+local TextLabel_4 = Instance.new("TextLabel")
+local TextLabel_5 = Instance.new("TextLabel")
+local TextLabel_6 = Instance.new("TextLabel")
+local TextLabel_7 = Instance.new("TextLabel")
+local TextLabel_8 = Instance.new("TextLabel")
+local TextLabel_9 = Instance.new("TextLabel")
+local TextLabel_10 = Instance.new("TextLabel")
+local MainText = Instance.new("TextLabel")
+local TextLabel_11 = Instance.new("TextLabel")
+local TextLabel_12 = Instance.new("TextLabel")
+local TextLabel_13 = Instance.new("TextLabel")
+local TextLabel_14 = Instance.new("TextLabel")
+local TextLabel_15 = Instance.new("TextLabel")
+local TextLabel_16 = Instance.new("TextLabel")
+local CloseEnd = Instance.new("TextButton")
+local TextLabel_17 = Instance.new("TextLabel")
+local TextLabel_18 = Instance.new("TextLabel")
+local TextLabel_19 = Instance.new("TextLabel")
+local TextLabel_20 = Instance.new("TextLabel")
+
+--Properties:
+
+ScreenGui.Parent = game.CoreGui
+
+Frame.Parent = ScreenGui
+Frame.Active = true
+Frame.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Frame.BorderColor3 = Color3.fromRGB(85, 0, 0)
+Frame.Position = UDim2.new(0.10693302, 0, 0.20408164, 0)
+Frame.Size = UDim2.new(0, 702, 0, 290)
+Frame.SizeConstraint = Enum.SizeConstraint.RelativeYY
+Frame.Style = Enum.FrameStyle.RobloxSquare
+Frame.Active = true
+Frame.Draggable = true
+
+TextLabel.Parent = Frame
+TextLabel.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel.BorderColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel.Position = UDim2.new(-0.012424957, 0, 0.875548899, 0)
+TextLabel.Size = UDim2.new(0, 171, 0, 42)
+TextLabel.Font = Enum.Font.Unknown
+TextLabel.Text = ";fly"
+TextLabel.TextColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel.TextSize = 15.000
+TextLabel.TextWrapped = true
+
+TextLabel_2.Parent = Frame
+TextLabel_2.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel_2.BorderColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_2.Position = UDim2.new(0.246834308, 0, 0.516928196, 0)
+TextLabel_2.Size = UDim2.new(0, 171, 0, 42)
+TextLabel_2.Font = Enum.Font.Unknown
+TextLabel_2.Text = ";farm"
+TextLabel_2.TextColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_2.TextSize = 15.000
+TextLabel_2.TextWrapped = true
+
+TextLabel_3.Parent = Frame
+TextLabel_3.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel_3.BorderColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_3.Position = UDim2.new(-0.0110004554, 0, 0.516928196, 0)
+TextLabel_3.Size = UDim2.new(0, 171, 0, 42)
+TextLabel_3.Font = Enum.Font.Unknown
+TextLabel_3.Text = ";viewer"
+TextLabel_3.TextColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_3.TextSize = 15.000
+TextLabel_3.TextWrapped = true
+
+TextLabel_4.Parent = Frame
+TextLabel_4.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel_4.BorderColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_4.Position = UDim2.new(0.762503803, 0, 0.703135133, 0)
+TextLabel_4.Size = UDim2.new(0, 171, 0, 42)
+TextLabel_4.Font = Enum.Font.Unknown
+TextLabel_4.Text = ";god giver"
+TextLabel_4.TextColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_4.TextSize = 15.000
+TextLabel_4.TextWrapped = true
+
+TextLabel_5.Parent = Frame
+TextLabel_5.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel_5.BorderColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_5.Position = UDim2.new(0.507518053, 0, 0.699686825, 0)
+TextLabel_5.Size = UDim2.new(0, 171, 0, 42)
+TextLabel_5.Font = Enum.Font.Unknown
+TextLabel_5.Text = ";Afk"
+TextLabel_5.TextColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_5.TextSize = 15.000
+TextLabel_5.TextWrapped = true
+
+TextLabel_6.Parent = Frame
+TextLabel_6.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel_6.BorderColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_6.Position = UDim2.new(0.246834323, 0, 0.699686885, 0)
+TextLabel_6.Size = UDim2.new(0, 171, 0, 42)
+TextLabel_6.Font = Enum.Font.Unknown
+TextLabel_6.Text = ";re"
+TextLabel_6.TextColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_6.TextSize = 15.000
+TextLabel_6.TextWrapped = true
+
+TextLabel_7.Parent = Frame
+TextLabel_7.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel_7.BorderColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_7.Position = UDim2.new(-0.0110004554, 0, 0.699686885, 0)
+TextLabel_7.Size = UDim2.new(0, 171, 0, 42)
+TextLabel_7.Font = Enum.Font.Unknown
+TextLabel_7.Text = ";Neptune"
+TextLabel_7.TextColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_7.TextSize = 15.000
+TextLabel_7.TextWrapped = true
+
+TextLabel_8.Parent = Frame
+TextLabel_8.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel_8.BorderColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_8.Position = UDim2.new(0.761079311, 0, 0.875548899, 0)
+TextLabel_8.Size = UDim2.new(0, 171, 0, 42)
+TextLabel_8.Font = Enum.Font.Unknown
+TextLabel_8.Text = ";kill all"
+TextLabel_8.TextColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_8.TextSize = 15.000
+TextLabel_8.TextWrapped = true
+
+TextLabel_9.Parent = Frame
+TextLabel_9.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel_9.BorderColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_9.Position = UDim2.new(0.246295974, 0, 0.875548899, 0)
+TextLabel_9.Size = UDim2.new(0, 171, 0, 42)
+TextLabel_9.Font = Enum.Font.Unknown
+TextLabel_9.Text = ";speed me 100"
+TextLabel_9.TextColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_9.TextSize = 15.000
+TextLabel_9.TextWrapped = true
+
+TextLabel_10.Parent = Frame
+TextLabel_10.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel_10.BorderColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_10.Position = UDim2.new(0.505132794, 0, 0.875548899, 0)
+TextLabel_10.Size = UDim2.new(0, 171, 0, 42)
+TextLabel_10.Font = Enum.Font.Unknown
+TextLabel_10.Text = ";gui"
+TextLabel_10.TextColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_10.TextSize = 15.000
+TextLabel_10.TextWrapped = true
+
+MainText.Name = "Main Text"
+MainText.Parent = Frame
+MainText.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+MainText.BorderColor3 = Color3.fromRGB(255, 255, 255)
+MainText.Position = UDim2.new(-0.00998731144, 0, -0.0301568396, 0)
+MainText.Size = UDim2.new(0, 665, 0, 30)
+MainText.Font = Enum.Font.Unknown
+MainText.Text = "Admin Commands are listed below - UI Version beta edition."
+MainText.TextColor3 = Color3.fromRGB(255, 255, 255)
+MainText.TextSize = 14.000
+
+TextLabel_11.Parent = Frame
+TextLabel_11.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel_11.BorderColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_11.Position = UDim2.new(0.765371978, 0, 0.33907038, 0)
+TextLabel_11.Size = UDim2.new(0, 165, 0, 42)
+TextLabel_11.Font = Enum.Font.Unknown
+TextLabel_11.Text = ";god"
+TextLabel_11.TextColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_11.TextSize = 15.000
+TextLabel_11.TextWrapped = true
+
+TextLabel_12.Parent = Frame
+TextLabel_12.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel_12.BorderColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_12.Position = UDim2.new(0.50466907, 0, 0.341066122, 0)
+TextLabel_12.Size = UDim2.new(0, 171, 0, 42)
+TextLabel_12.Font = Enum.Font.Unknown
+TextLabel_12.Text = ";sword god"
+TextLabel_12.TextColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_12.TextSize = 15.000
+TextLabel_12.TextWrapped = true
+
+TextLabel_13.Parent = Frame
+TextLabel_13.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel_13.BorderColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_13.Position = UDim2.new(0.245409831, 0, 0.341066122, 0)
+TextLabel_13.Size = UDim2.new(0, 171, 0, 42)
+TextLabel_13.Font = Enum.Font.Unknown
+TextLabel_13.Text = "credits"
+TextLabel_13.TextColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_13.TextSize = 15.000
+TextLabel_13.TextWrapped = true
+
+TextLabel_14.Parent = Frame
+TextLabel_14.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel_14.BorderColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_14.Position = UDim2.new(-0.0110004554, 0, 0.341066122, 0)
+TextLabel_14.Size = UDim2.new(0, 171, 0, 42)
+TextLabel_14.Font = Enum.Font.Unknown
+TextLabel_14.Text = ";fastdupe"
+TextLabel_14.TextColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_14.TextSize = 15.000
+TextLabel_14.TextWrapped = true
+
+TextLabel_15.Parent = Frame
+TextLabel_15.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel_15.BorderColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_15.Position = UDim2.new(0.762503803, 0, 0.516928196, 0)
+TextLabel_15.Size = UDim2.new(0, 171, 0, 42)
+TextLabel_15.Font = Enum.Font.Unknown
+TextLabel_15.Text = ";utg"
+TextLabel_15.TextColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_15.TextSize = 15.000
+TextLabel_15.TextWrapped = true
+
+TextLabel_16.Parent = Frame
+TextLabel_16.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel_16.BorderColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_16.Position = UDim2.new(0.50466907, 0, 0.516928256, 0)
+TextLabel_16.Size = UDim2.new(0, 171, 0, 42)
+TextLabel_16.Font = Enum.Font.Unknown
+TextLabel_16.Text = ";help"
+TextLabel_16.TextColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_16.TextSize = 15.000
+TextLabel_16.TextWrapped = true
+
+CloseEnd.Name = "Close/End"
+CloseEnd.Parent = Frame
+CloseEnd.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+CloseEnd.Position = UDim2.new(0.960336089, 0, -0.0340234824, 0)
+CloseEnd.Size = UDim2.new(0, 35, 0, 39)
+CloseEnd.Style = Enum.ButtonStyle.RobloxButtonDefault
+CloseEnd.Font = Enum.Font.Creepster
+CloseEnd.Text = "X"
+CloseEnd.TextColor3 = Color3.fromRGB(255, 255, 255)
+CloseEnd.TextSize = 14.000
+
+TextLabel_17.Parent = Frame
+TextLabel_17.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel_17.BorderColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_17.Position = UDim2.new(-0.0124249859, 0, 0.161755845, 0)
+TextLabel_17.Size = UDim2.new(0, 171, 0, 42)
+TextLabel_17.Font = Enum.Font.Unknown
+TextLabel_17.Text = ";fling"
+TextLabel_17.TextColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_17.TextSize = 15.000
+TextLabel_17.TextWrapped = true
+
+TextLabel_18.Parent = Frame
+TextLabel_18.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel_18.BorderColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_18.Position = UDim2.new(0.75965482, 0, 0.165204093, 0)
+TextLabel_18.Size = UDim2.new(0, 171, 0, 42)
+TextLabel_18.Font = Enum.Font.Unknown
+TextLabel_18.Text = ";effects"
+TextLabel_18.TextColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_18.TextSize = 15.000
+TextLabel_18.TextWrapped = true
+
+TextLabel_19.Parent = Frame
+TextLabel_19.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel_19.BorderColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_19.Position = UDim2.new(0.501820028, 0, 0.165204108, 0)
+TextLabel_19.Size = UDim2.new(0, 171, 0, 42)
+TextLabel_19.Font = Enum.Font.Unknown
+TextLabel_19.Text = ";crasher"
+TextLabel_19.TextColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_19.TextSize = 15.000
+TextLabel_19.TextWrapped = true
+
+TextLabel_20.Parent = Frame
+TextLabel_20.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel_20.BorderColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_20.Position = UDim2.new(0.245409757, 0, 0.158307567, 0)
+TextLabel_20.Size = UDim2.new(0, 171, 0, 42)
+TextLabel_20.Font = Enum.Font.Unknown
+TextLabel_20.Text = ";Discord - Down"
+TextLabel_20.TextColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_20.TextSize = 15.000
+TextLabel_20.TextWrapped = true
+
+-- Scripts:
+
+local function NDESTA_fake_script() -- CloseEnd.LocalScript 
+	local script = Instance.new('LocalScript', CloseEnd)
+
+	script.Parent.MouseButton1Click:Connect(function()
+		script.Parent.Parent.Visible = false
+	end)
+end
+coroutine.wrap(NDESTA_fake_script)()
